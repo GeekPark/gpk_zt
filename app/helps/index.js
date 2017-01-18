c00032f4cb1f6540fe94953d0afc09d42e98e15b
Cedcn <cedcn@qq.com>
2017-01-19 03:23:41 +0800

    fix bug - method err

--- app/helps/index.js
+++ app/helps/index.js
@@ -3,10 +3,12 @@ import fs from 'fs';
 export const includeWebpackScripts = (...opt) => {
   const generateHtmlStr = key => {
     try {
-      const manifest = fs.readFileSync('../public/webpack_manifest.json');
-      return `<script text="text/javascript" src="/assets/js/${manifest[key]}.js"></script>`;
+      const manifest = fs.readFileSync('public/assets/js/webpack_manifest.json');
+      const m = JSON.parse(manifest.toString());
+      return `<script text="text/javascript" src="/assets/js/${m[key]}"></script>`;
     } catch (e) {
-      return `<script text="text/javascript" src="/assets/${key}.js"></script>`;
+      console.log(e);
+      return `<script text="text/javascript" src="/assets/${key}"></script>`;
     }
   };
 
