c1f4d165bf9577e81d8c5ae285cf5f0fbb50024f
Cedcn <cedcn@qq.com>
2017-01-20 01:57:30 +0800

    browser harmony

--- app/helps/index.js
+++ app/helps/index.js
@@ -7,7 +7,6 @@ exports.includeWebpackScripts = (...opt) => {
       const m = JSON.parse(manifest.toString());
       return `<script text="text/javascript" src="/assets/js/${m[key]}"></script>`;
     } catch (e) {
-      console.log(e);
       return `<script text="text/javascript" src="/assets/${key}"></script>`;
     }
   };
